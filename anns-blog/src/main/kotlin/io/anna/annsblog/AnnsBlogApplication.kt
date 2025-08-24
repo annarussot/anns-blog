04a65afb5bc643c1af550a38286715730485e081
endernaya <annarussops@gmail.com>
2025-08-24 13:56:53 +0200

    Configuration

---
+++ anns-blog/src/main/kotlin/io/anna/annsblog/AnnsBlogApplication.kt
@@ -0,0 +1,11 @@
+package io.anna.annsblog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnnsBlogApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnnsBlogApplication>(*args)
+}
---
+++ anns-blog/src/test/kotlin/io/anna/annsblog/AnnsBlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.anna.annsblog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnnsBlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
